47057d4dec342d419d342f7995aecc7caff5fc29
Andres Orozco <andres.orozco97@hotmail.com>
2021-05-02 22:20:04 -0500

    Upload files of atomic architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ControlComponent } from './shared/molecules/control/control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ControlComponent
   ],
   imports: [
     BrowserModule,
